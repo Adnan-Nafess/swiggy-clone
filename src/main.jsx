bf5810ed7a057a186840887a1d04bd5d4ed282d6
Adnan Nafees <mohdadnan3508.com>
2025-04-08 01:45:19 +0530

    Fix: updated import path for locationSearchVisibilitySlice

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
-import store from "./utils/store";
+import store from "./Utils/store.js";
 import './index.css'
 import App from './App.jsx'
 
